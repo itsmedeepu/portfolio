ea0a8c554f40cb3ca68808f54ca9144960ef05ed
Deepak s <itsmedeepu2021@gmail.com>
2021-11-28 10:19:42 +0530

    my intial commit

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+//particle js
+
+particlesJS.load("particles-js", "../particle/particles.json", function () {
+  console.log("callback - particles.js config loaded");
+});
+
+let mybutton = document.getElementById("back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
